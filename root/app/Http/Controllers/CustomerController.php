42b137ce535f83077809484472d2636bd518d2a9
Iwata Sou <so.iwata@epkotsoftware.co.jp>
2025-06-09 18:30:07 +0900

    コントローラ修正

--- root/app/Http/Controllers/CustomerController.php
+++ root/app/Http/Controllers/CustomerController.php
@@ -16,37 +16,37 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        $result = DB::table('customers as c')
-        ->join('interactions as i', 'i.customer_id', '=', 'c.id')
-        ->join('users as u', 'i.user_id', '=', 'u.id')
-        ->select(
-            'c.id as customer_id',
-            'c.name as customer_name',
-            'c.updated_at as customer_updated_at',
-            'i.content AS interaction_content',
-            'u.id as user_id',
-            'u.name as user_name'
-        )
-        ->get();
+        // $result = DB::table('customers as c')
+        // ->join('interactions as i', 'i.customer_id', '=', 'c.id')
+        // ->join('users as u', 'i.user_id', '=', 'u.id')
+        // ->select(
+        //     'c.id as customer_id',
+        //     'c.name as customer_name',
+        //     'c.updated_at as customer_updated_at',
+        //     'i.content AS interaction_content',
+        //     'u.id as user_id',
+        //     'u.name as user_name'
+        // )
+        // ->get();
 
-        $grouped = $result->groupBy('customer_id')
-                    ->map(function ($items) {
-                        return [
-                            'customer_name' => $items->first()->customer_name,
-                            'updated_at' => $items->max('customer_updated_at'),
-                            'content' => $items->map(function($item) {
-                                return [
-                                    'content' => $item->interaction_content,
-                                ];
-                            }),
-                            'users' => $items->map(function ($item) {
-                                return [
-                                    'user_id' => $item->user_id,
-                                    'user_name' => $item->user_name,
-                                ];
-                            }),
-                        ];
-                    });
+        // $grouped = $result->groupBy('customer_id')
+        //             ->map(function ($items) {
+        //                 return [
+        //                     'customer_name' => $items->first()->customer_name,
+        //                     'updated_at' => $items->max('customer_updated_at'),
+        //                     'content' => $items->map(function($item) {
+        //                         return [
+        //                             'content' => $item->interaction_content,
+        //                         ];
+        //                     }),
+        //                     'users' => $items->map(function ($item) {
+        //                         return [
+        //                             'user_id' => $item->user_id,
+        //                             'user_name' => $item->user_name,
+        //                         ];
+        //                     }),
+        //                 ];
+        //             });
 
         return Inertia::render('Customers/Index', [
             'customers' => Customer::select('id', 'name', 'email', 'phone', 'company_name', 'status', 'updated_at')->paginate(10)
